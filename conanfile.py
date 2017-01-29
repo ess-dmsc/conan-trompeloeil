13fa4f7b74b911335843c18bbddfa09e3806ae73
Bjorn Fahller <bjorn@fahller.se>
2017-01-29 18:45:51 +0100

    Update for stable v23

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class TrompeloeilConan(ConanFile):
     name = "trompeloeil"
-    version = "v22"
+    version = "v23"
     license = "Boost"
     author = "Bjorn Fahller (bjorn@fahller.se)"
     url = "https://github.com/rollbear/trompeloeil-conan.git"
@@ -12,7 +12,7 @@ class TrompeloeilConan(ConanFile):
         None # header only
 
     def source(self):
-        self.run("git clone https://github.com/rollbear/trompeloeil.git --branch v22")
+        self.run("git clone https://github.com/rollbear/trompeloeil.git --branch v23")
 
     def package(self):
         self.copy("*.hpp", dst="include", src="trompeloeil")
